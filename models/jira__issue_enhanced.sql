95b07b16f5c435268a9f07a4a42d6202d268d44d
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-12-15 17:56:19 -0500

    some date diff stuff

--- models/jira__issue_enhanced.sql
+++ models/jira__issue_enhanced.sql
@@ -32,7 +32,12 @@ final as (
         issue.*,
         issue_dates.first_assigned_at,
         issue_dates.last_assigned_at,
-        issue_dates.first_resolved_at
+        issue_dates.first_resolved_at,
+
+        {{ dbt_utils.datediff('issue.created_at', "coalesce(issue_dates.first_assigned_at, " ~ dbt_utils.current_timestamp() ~ ')', 'second') }} unassigned_duration_seconds,
+        {{ dbt_utils.datediff('issue.created_at', "coalesce(issue.resolved_at, " ~ dbt_utils.current_timestamp() ~ ')', 'second') }} open_duration_seconds,
+        -- this will be null if no one has been assigned
+        {{ dbt_utils.datediff('issue_dates.last_assigned_at', "coalesce(issue.resolved_at, " ~ dbt_utils.current_timestamp() ~ ')', 'second') }} last_assignment_duration_seconds 
     
     from issue left join issue_dates using(issue_id)
         
