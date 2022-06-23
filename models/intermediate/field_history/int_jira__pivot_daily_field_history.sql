05d0db5d17c614299ed97e5ed9ab1b0b5f377d18
Troy Schuetrumpf <troy.schuetrumpf@elationhealth.com>
2022-06-23 16:57:50 +1200

    Updated status to also use field name and remove redundant comment

--- models/intermediate/field_history/int_jira__pivot_daily_field_history.sql
+++ models/intermediate/field_history/int_jira__pivot_daily_field_history.sql
@@ -28,8 +28,8 @@ pivot_out as (
     select 
         valid_starting_on, 
         issue_id,
-        max(case when lower(field_id) = 'status' then field_value end) as status,
-        max(case when lower(field_name) = 'sprint' then field_value end) as sprint -- As sprint is a custom field, we aggregate on the field_name.
+        max(case when lower(field_name) = 'status' then field_value end) as status,
+        max(case when lower(field_name) = 'sprint' then field_value end) as sprint
 
         {% for col in var('issue_field_history_columns', []) -%}
         ,
