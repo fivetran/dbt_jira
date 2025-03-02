3caa87938bb41df87277014ffedbc0e482b1ecf9
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-03-02 15:04:21 -0500

    remove unified rag

--- macros/count_tokens.sql
+++
@@ -1,7 +0,0 @@
-{% macro count_tokens(column_name) -%}
-  {{ return(adapter.dispatch('count_tokens', 'jira')(column_name)) }}
-{%- endmacro %}
-
-{% macro default__count_tokens(column_name) %}
-  {{ dbt.length(column_name) }} / 4
-{% endmacro %}
