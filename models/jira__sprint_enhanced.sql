d5fa93de5b1db6424ac6f1076c9712800a0a3941
Avinash Kunnath <avinash.kunnath@fivetran.com>
2026-01-22 12:54:36 -0800

    Revert "fix grain of sprint_enhanced"
    This reverts commit 087820649c252980b70b398304d3ffb7aa51b6b1.

--- models/jira__sprint_enhanced.sql
+++ models/jira__sprint_enhanced.sql
@@ -26,9 +26,8 @@ sprint_metrics_grouped as (
 sprint_issue_metrics as (
 
     select
-        source_relation,
         sprint_id,
-        {{ "team," if using_teams }}
+        source_relation,
         count(distinct issue_id) as sprint_issues,
         count(distinct assignee_user_id) as sprint_assignees,
         count(distinct (case when is_sprint_active and is_issue_open then issue_id end)) as open_sprint_issues,
@@ -36,15 +35,14 @@ sprint_issue_metrics as (
             and issue_resolved_at <= sprint_ended_at
             then issue_id end)) as resolved_sprint_issues
     from daily_sprint_issue_history
-    {{ dbt_utils.group_by(3 if using_teams else 2) }}
-)
+    {{ dbt_utils.group_by(2) }}
+),
 
 sprint_start_metrics as (
 
     select
-        source_relation,
         sprint_id,
-        {{ "team," if using_teams }}
+        source_relation,
         sum(case when story_points is null then 0 else story_points end) as story_points_committed,
         sum(case when story_point_estimate is null then 0 else story_point_estimate end) as story_point_estimate_committed,
         count(distinct issue_id) as issues_committed
@@ -53,22 +51,21 @@ sprint_start_metrics as (
     where date_day = cast(sprint_started_at as date)
         or (date_day < cast(sprint_started_at as date)
             and date_day = cast({{ dbt.current_timestamp() }} as date))
-    {{ dbt_utils.group_by(3 if using_teams else 2) }}
+    {{ dbt_utils.group_by(2) }}
 ),
 
 sprint_end_metrics as (
 
     select
-        source_relation,
         sprint_id,
-        {{ "team," if using_teams }}
+        source_relation,
         sum(case when story_points is null then 0 else story_points end) as story_points_end,
         sum(case when story_point_estimate is null then 0 else story_point_estimate end) as story_point_estimate_end,
         sum(case when is_issue_resolved_in_sprint then story_points else 0 end) as story_points_completed,
         sum(case when is_issue_resolved_in_sprint then story_point_estimate else 0 end) as story_point_estimate_completed
     from daily_sprint_issue_history
     where date_day = cast(sprint_ended_at as date)
-    {{ dbt_utils.group_by(3 if using_teams else 2) }}
+    group by 1, 2
 ), 
 
 final as (
@@ -99,15 +96,12 @@ final as (
     left join sprint_issue_metrics
         on sprint_metrics_grouped.sprint_id = sprint_issue_metrics.sprint_id
         and sprint_metrics_grouped.source_relation = sprint_issue_metrics.source_relation
-        {{ "and sprint_metrics_grouped.team = sprint_issue_metrics.team" if using_teams }}
     left join sprint_start_metrics
         on sprint_metrics_grouped.sprint_id = sprint_start_metrics.sprint_id
         and sprint_metrics_grouped.source_relation = sprint_start_metrics.source_relation
-        {{ "and sprint_metrics_grouped.team = sprint_start_metrics.team" if using_teams }}
     left join sprint_end_metrics
         on sprint_metrics_grouped.sprint_id = sprint_end_metrics.sprint_id
         and sprint_metrics_grouped.source_relation = sprint_end_metrics.source_relation
-        {{ "and sprint_metrics_grouped.team = sprint_end_metrics.team" if using_teams }}
     {{ dbt_utils.group_by(18) }}
 )
 
